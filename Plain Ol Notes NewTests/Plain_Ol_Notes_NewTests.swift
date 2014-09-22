514c7470e62627f6ce8a7fc0c1a1600d3fedd3d9
Yee Wong <ywonggit@gmail.com>
2014-09-22 14:58:25 +0100

    Initial Commit

---
+++ Plain Ol Notes NewTests/Plain_Ol_Notes_NewTests.swift
@@ -0,0 +1,36 @@
+//
+//  Plain_Ol_Notes_NewTests.swift
+//  Plain Ol Notes NewTests
+//
+//  Created by Yee Wong on 22/09/2014.
+//  Copyright (c) 2014 Yee Wong. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Plain_Ol_Notes_NewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
